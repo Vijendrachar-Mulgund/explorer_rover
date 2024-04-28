d9c8b304ad81581899c0a4bddda80213c749eff6
vijendrachar-mulgund <vijendrachar.m@gmail.com>
2024-04-28 17:20:17 +0100

    The gazebo launch file has been added!

---
+++ launch/simulation.launch.py
@@ -0,0 +1,39 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+
+# Main File
+def generate_launch_description():
+    # Name of the Ros Package
+    package_name='explorer_rover' 
+
+    # Launch the RSP launch file and pass the use_sim_time argument to sync the time
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+             )
+
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_rover'],
+                        output='screen')
+
+    # Launch All the Nodes
+    return LaunchDescription([
+        rsp,
+        gazebo,
+        spawn_entity,
+    ])
